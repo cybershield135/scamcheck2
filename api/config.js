933cfef5152c7304d8f261ef957bdaf20341e13f
cybershield135 <hytesting56@gmail.com>
2026-06-20 13:46:03 +0700

    Fix config

---
+++ api/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    GEMINI_API_KEY: env_local.GEMINI_API_KEY,
+    API_URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
+};
+
+export default CONFIG;
